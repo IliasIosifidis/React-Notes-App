3a4ebcd089b32a07a2e6858f6ec4fcd5f7ea6de6
ilias iosifidis <iosifidis90@gmail.com>
2025-10-18 10:34:32 +0300

    Merge remote-tracking branch 'origin/master'

--- notes-app/src/components/NoteList.tsx
+++ notes-app/src/components/NoteList.tsx
@@ -1,3 +1,5 @@
+import Note from "./Note.tsx";
+
 const NoteList = ({notes, deleteNote}) => {
     if (notes.length === 0) {
         return (
@@ -5,27 +7,14 @@ const NoteList = ({notes, deleteNote}) => {
                 No Notes Yet
             </p>
         )
-
     }
     return (
         <div className="space-y-4">
             {notes.map((note) => (
-                <div
-                    key={note.id}
-                    className='p-4 bg-white rounded-lg shadow-md border-l-4'>
-                    <h3 className='text-lg font-bold' >{note.title}</h3>
-                    <p className='text-sm text-gray-600'>
-                        <strong>Category: </strong> {note.category}
-                    </p>
-                    <p className={'mt-2'}>{note.description}</p>
-
-                    <button onClick={() => deleteNote(note.id)}
-                            className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700">Delete</button>
-                </div>
+               <Note key={note.id note={note} deleteNote={deleteNote}/>
             ))}
         </div>
     )
-
 }
 
 export default NoteList
