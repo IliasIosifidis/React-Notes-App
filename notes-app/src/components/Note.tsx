228d4c7b5b7d6431d0f0ed49135b5c4688f67a50
ilias iosifidis <iosifidis90@gmail.com>
2025-10-18 10:51:18 +0300

    Merge remote-tracking branch 'origin/master'

--- notes-app/src/components/Note.tsx
+++ notes-app/src/components/Note.tsx
@@ -1,7 +1,7 @@
 const Note = ({note, deleteNote}) => {
     return (
         <div
-            className='p-4 bg-white rounded-lg shadow-md border-l-4'>
+            className='p-4 bg-white rounded-lg shadow-md  border-l-4 border-blue-500'>
             <h3 className='text-lg font-bold' >{note.title}</h3>
             <p className='text-sm text-gray-600'>
                 <strong>Category: </strong> {note.category}
--- notes-app/src/components/NoteList.tsx
+++ notes-app/src/components/NoteList.tsx
@@ -1,20 +1,38 @@
+import React from "react";
 import Note from "./Note.tsx";
 
-const NoteList = ({notes, deleteNote}) => {
+interface NoteType {
+    id: string | number;
+    title: string;
+    category: string;
+    description: string;
+}
+
+interface NoteListProps {
+    notes: NoteType[];
+    deleteNote: (id: string | number) => void;
+}
+
+const NoteList: React.FC<NoteListProps> = ({ notes, deleteNote }) => {
     if (notes.length === 0) {
         return (
             <p className="text-center text-gray-500">
                 No Notes Yet
             </p>
-        )
+        );
     }
+
     return (
         <div className="space-y-4">
             {notes.map((note) => (
-               <Note key={note.id note={note} deleteNote={deleteNote}/>
+                <Note
+                    key={note.id}
+                    note={note}
+                    deleteNote={deleteNote}
+                />
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default NoteList
+export default NoteList;
